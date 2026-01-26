5a1583101ab85b55514b79fa011735c0c8ecbfba
Sava3355 <t125011@ed.sus.ac.jp>
2026-01-26 11:36:45 +0900

    グラフ表示の試用

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import pandas as pd
 
+st.title('大学 文理別就職内定率')
+df = pd.read_csv('FEH_00400402_260126111959.csv')
+
+with st.sidebar:
+    year = st.multiselect('年を選択してください。',
+                          df['時間軸(12月)'].unique())
+
+
+st.dataframe(df,width=800, height=220)
